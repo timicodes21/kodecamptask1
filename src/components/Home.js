43b37109ab9f164f2cfea4c6dd344802a53ad128
timicodes21 <timileyinbabalola42@gmail.com>
2022-04-05 10:17:42 +0100

    giftcards

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,27 @@ export class Home extends Component {
   render() {
       console.log(allGiftCards)
     return (
-      <div className="bg-gray">
+      <div>
           <Container className="p-5">
-                <h3 className="text-orange font-40">Giftcards</h3>
-                <Row className="my-5">
-
+                <h3 className="text-black font-40">Giftcards</h3>
+                <Row className="my-3 bg-gray">
+                    {
+                        allGiftCards.map(giftcard => (
+                            <div key={giftcard.id} className="col-12 col-md-3 bg-white shadow-sm p-3 my-2 rounded">
+                                <p className="text-orange font-32">{giftcard.title}</p>
+                                <h4 className="text-black">Currencies</h4>
+                                <Row> 
+                                    {
+                                        giftcard.currencies.map(currency => (
+                                            <div className="col-3">
+                                                <p className="text-blue font-16">{currency}</p>
+                                            </div>
+                                        ))
+                                    }
+                                </Row>
+                            </div>
+                        ))
+                    }
                 </Row>
           </Container>
       </div>
